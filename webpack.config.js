223c3c30e67cd5ac3c45b50aef8351ff6f5a34ac
thysmile <mrrthy0503@gmail.com>
2022-12-01 14:52:49 +0700

    push

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-require("babel-polyfill")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+require("babel-polyfill");
 module.exports = {
-  entry: ['babel-polyfill', __dirname + '/src/app/index.js'],
+  entry: ["babel-polyfill", __dirname + "/src/app/index.js"],
   output: {
-   path: __dirname + '/dist',
-   filename: 'bundle.js',
-   publicPath: '/'
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+    publicPath: "/",
   },
   module: {
-   rules: [
-   {
-   test: /\.js$/,
-   use: 'babel-loader',
-   exclude: [/node_modules/]
-},
-{
-  test: /\.(sa|sc|c)ss$/,
-  use: [{
-  loader: 'style-loader'
-  }, {
-  loader: 'css-loader'
-  }, {
-  loader: 'sass-loader'
-  }]
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [{ loader: "css-loader" }, { loader: "sass-loader" }],
+        }),
+      },
+    ],
   },
-  {
-  test: /\.css$/,
-  use: ExtractTextPlugin.extract({
-  fallback: 'style-loader',
-  use: [
-  { loader: 'css-loader'},
-  { loader: 'sass-loader'}
-  ],
-  })
-  }
-  ]
-},
   plugins: [
-  new HtmlWebpackPlugin({
-  template: __dirname + '/src/public/index.html',
-  inject: 'body'
-  }),
-  new ExtractTextPlugin('main.css')
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/public/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+    new ExtractTextPlugin("main.css"),
   ],
   devServer: {
-  contentBase: './src/public',
-  port: 8000
-  }
-}
+    contentBase: "./src/public",
+    port: 8000,
+  },
+};
